90bf3aea0975d29b10aa5a58b20bec287f780531
Jamal Khalili <j.khalili15@yahoo.com>
2023-12-29 19:14:14 +0400

    layout: setup logic

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,16 +17,35 @@
 
 <script setup lang="ts">
 import { useApp } from "~/store/useApp";
+import { useLoader } from "~/store/useLoader";
+import { useTheme } from "~/store/useTheme";
 
-const { setIsMainHomeActive } = useApp();
+const { isDataLoaded, setIsMainHomeActive } = useApp();
+const { isLoaderActive } = useLoader();
+const theme = useTheme();
+const { isDarkTheme } = storeToRefs(theme);
 
 const $container = ref<HTMLElement>();
 
-const isLoaderActive = false;
+const setProperty = (key: string, value: string) => {
+    document.documentElement.style.setProperty(key, value);
+};
+
+const setTheme = (isDark: boolean = false) => {
+    const primary = isDark ? "negative" : "primary";
+    const negative = !isDark ? "negative" : "primary";
+
+    setProperty("--primary", theme[primary]);
+    setProperty("--negative", theme[negative]);
+    setProperty("--primary-transparent", `${theme[primary]}a`);
+    setProperty("--negative-transparent", `${theme[negative]}a`);
+
+    document.body.classList.add(`is-${isDark ? "dark" : "light"}-theme`);
+    document.body.classList.remove(`is-${isDark ? "light" : "dark"}-theme`);
+};
 
 const handleScroll = () => {
     const top = $container.value?.scrollTop || 0;
-
     setIsMainHomeActive(top < 100);
 };
 
@@ -34,7 +53,13 @@ const initialiseEventListeners = () => {
     $container.value?.addEventListener("scroll", handleScroll);
 };
 
+watch(isDarkTheme, (value) => {
+    setTheme(value);
+    console.log("isDarkTheme changed: ", isDarkTheme);
+});
+
 onMounted(() => {
+    setTheme(isDarkTheme.value);
     initialiseEventListeners();
 });
 
---
+++ layouts/project.vue
@@ -0,0 +1,22 @@
+<style lang="scss" land="sass" src="./scss/layout.scss"></style>
+
+<template>
+    <div
+        :class="[
+            'b-layout',
+            {
+                'is-loader-active': isLoaderActive,
+            },
+        ]"
+    >
+        <div ref="$container" class="layout-container">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useLoader } from "~/store/useLoader";
+
+const { isLoaderActive } = useLoader();
+</script>
