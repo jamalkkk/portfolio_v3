adf67c1a9b464b72e0634d4f7363ce9924592c62
Jamal Khalili <j.khalili15@yahoo.com>
2024-05-24 16:34:19 +0200

    remove logs

--- components/block/projects/Projects.vue
+++ components/block/projects/Projects.vue
@@ -85,8 +85,6 @@ const resetProjects = (value = activeTags.value) => {
     if (value.length) {
         const filteredProjectsArr: SBProjectDetails[] = [];
 
-        console.log("projects all of", allProjects.value);
-
         allProjects.value.forEach((project) => {
             if (activeTags.value.some((tag) => project.tags.includes(tag))) {
                 filteredProjectsArr.push(project);
@@ -94,10 +92,8 @@ const resetProjects = (value = activeTags.value) => {
         });
 
         filteredProjects.value = sortProjects(filteredProjectsArr);
-        console.log("projects fitlerd", filteredProjects.value);
     } else {
         filteredProjects.value = sortProjects(allProjects.value);
-        console.log("projects all", filteredProjects.value);
     }
 };
 
